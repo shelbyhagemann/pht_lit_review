bfd732e8a858ab231f911b8b302048e74a0d8792
Shelby Hagemann <52891544+shelbyhagemann@users.noreply.github.com>
2025-07-24 21:41:19 -0700

    Adding descriptions for functions

--- code/clean_convidence.py
+++ code/clean_convidence.py
@@ -1,9 +1,44 @@
 ## References:
 
 ## Import libraries
+import pandas as pd
 
 ## Declare global variables
 
 ## Declare functions
 
+# Function Name: load_file
+# Inputs: CSV file path
+# Outputs: dataframe, pandas df of csv data
+# Description: Uses pandas library to convert CSV file into dataframe
+def load_file(file_path):
+    pass
+
+# Function Name: remove_extra_cols
+# Inputs: dataframe
+# Outputs: dataframe (revised df)
+# Description: Some columns are included twice, like the paper title. 
+def remove_extra_cols(dataframe):
+    pass
+
+# Function Name:
+# Inputs:
+# Outputs:
+# Description:
+
+# Function Name:
+# Inputs:
+# Outputs:
+# Description:
+
+# Function Name:
+# Inputs:
+# Outputs:
+# Description:
+
 ## Main function
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
